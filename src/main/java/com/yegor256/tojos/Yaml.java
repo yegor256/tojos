3b48bb2e831eb450a9276e009cdfd743f4a31810
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-06-17 08:37:19 +0500

    Update Yaml.java
    Refactor to meet style requirements

--- src/main/java/com/yegor256/tojos/Yaml.java
+++ src/main/java/com/yegor256/tojos/Yaml.java
@@ -1,6 +1,32 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2021 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.yegor256.tojos;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -8,25 +34,29 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * YAML file
+ * YAML file.
+ * @since 0.9.2
  */
 public final class Yaml implements Mono {
-    private final File destination;
+    /**
+     * Where the YAML is stored.
+     */
+    private final Path destination;
 
     /**
      * Ctor.
-     * @param file Destination where to keep the data
+     * @param path Destination where to keep the data
      */
-    public Yaml(final File file) {
-        this.destination = file;
+    public Yaml(final Path path) {
+        this.destination = path;
     }
 
     /**
      * Ctor.
      * @param path Path to the file
      */
-    public Yaml(final Path path) {
-        this(path.toFile());
+    public Yaml(final File path) {
+        this(path.toPath());
     }
 
     /**
@@ -39,37 +69,30 @@ public final class Yaml implements Mono {
 
     @Override
     public Collection<Map<String, String>> read() {
-        if (!this.destination.exists()) {
-            return new LinkedList<>();
-        }
+        final Collection<Map<String, String>> result = new LinkedList<>();
         try {
-            final InputStream source = new FileInputStream(this.destination);
-            return new org.yaml.snakeyaml.Yaml().<List<Map<String, String>>>load(source);
-        } catch (final IOException e) {
+            final InputStream source = Files.newInputStream(this.destination);
+            result.addAll(new org.yaml.snakeyaml.Yaml().<List<Map<String, String>>>load(source));
+        } catch (final IOException exception) {
             throw new IllegalArgumentException(
-                String.format("Failed to read YAML from '%s'", this.destination)
+                String.format("Failed to read YAML from '%s'", this.destination),
+                exception
             );
         }
+        return result;
     }
 
     @Override
     public void write(final Collection<Map<String, String>> rows) {
         try {
-            if (!destination.exists()) {
-                if (!destination.createNewFile()) {
-                    throw new IOException(
-                        String.format("Failed to create file '%s'", this.destination)
-                    );
-                }
-            }
             new org.yaml.snakeyaml.Yaml().dump(
                 rows,
-                new PrintWriter(this.destination)
+                Files.newBufferedWriter(this.destination)
             );
-        } catch (IOException e) {
+        } catch (final IOException exception) {
             throw new IllegalArgumentException(
                 String.format("Failed to write YAML to '%s'", this.destination),
-                e
+                exception
             );
         }
     }
--- src/test/java/com/yegor256/tojos/YamlTest.java
+++ src/test/java/com/yegor256/tojos/YamlTest.java
@@ -1,9 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2021 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.yegor256.tojos;
-
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -11,7 +29,15 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+/**
+ * Test case for {@link Yaml}.
+ * @since 0.9.2
+ */
 public class YamlTest {
 
     @Test
@@ -19,14 +45,15 @@ public class YamlTest {
         final File file = temp.resolve("test.yml").toFile();
         final Map<String, String> keys = new HashMap<>();
         final List<Map<String, String>> tojo = new LinkedList<>();
+        final String key = "key";
         tojo.add(keys);
-        keys.put("key", "value");
+        keys.put(key, "value");
         final Mono yaml = new Yaml(file);
         yaml.write(tojo);
         MatcherAssert.assertThat(
-            keys.get("key"),
+            keys.get(key),
             Matchers.equalTo(
-                yaml.read().stream().findFirst().get().get("key")
+                yaml.read().stream().findFirst().get().get(key)
             )
         );
     }
