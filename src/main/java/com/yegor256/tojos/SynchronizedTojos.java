b668d94ac690555b6c30984588cc6a9776718c05
Ivan <clicker.heroes.acg@gmail.com>
2022-07-31 19:06:39 +0300

    try to #16

---
+++ src/main/java/com/yegor256/tojos/SynchronizedTojos.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2022 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.yegor256.tojos;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Tojos thread-safe implementation.
+ *
+ * The class is thread-safe.
+ *
+ * @since 0.3.0
+ */
+public class SynchronizedTojos implements Tojos {
+
+    /**
+     * The wrapped.
+     */
+    private final Tojos wrapped;
+
+    /**
+     * Synchronized collection.
+     */
+    private final Collection<Tojo> sync;
+
+    /**
+     * Ctor.
+     *
+     * @param wrapped The Tojos to decorate
+     */
+    public SynchronizedTojos(final Tojos wrapped) {
+        this.wrapped = wrapped;
+        this.sync = Collections.synchronizedCollection(wrapped.select(t -> true));
+    }
+
+    @Override
+    public final Tojo add(final String id) {
+        try {
+            final Tojo tojo = this.wrapped.add(id);
+            this.sync.add(tojo);
+            return tojo;
+        } catch (final ConcurrentModificationException exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+
+    @Override
+    public final List<Tojo> select(final Predicate<Tojo> filter) {
+        if (this.sync.isEmpty()) {
+            this.sync.addAll(this.wrapped.select(t -> true));
+        }
+        return this.sync.stream()
+            .filter(filter)
+            .collect(Collectors.toList());
+    }
+
+}
