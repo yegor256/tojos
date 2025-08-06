e17aadd4f79aaa32d92f8e6e66b9bc3e1cd879f2
Jahongir <tvajoyibgoya@gmail.com>
2025-08-06 06:46:54 +0500

    fix concurrentModificationException in MnCsv.dup

--- src/main/java/com/yegor256/tojos/MnCsv.java
+++ src/main/java/com/yegor256/tojos/MnCsv.java
@@ -15,7 +15,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * CSV file.
